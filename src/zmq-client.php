178ffdd8ea927dad6c54a5809530da8ed3435414
vantt <vantt@anphabe.com>
2015-11-19 17:58:40 +0700

    add testing on ZMQ Request/Reply

--- src/zmq-client.php
+++ src/zmq-client.php
@@ -1,16 +1,15 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__.'/../vendor/autoload.php';
-
-$loop = React\EventLoop\Factory::create();
-$context = new React\ZMQ\Context($loop);
-$socket = $context->getSocket(ZMQ::SOCKET_REQ);
+$context = new ZMQContext();
+$socket  = $context->getSocket(ZMQ::SOCKET_REQ);
 $socket->connect('tcp://127.0.0.1:1337');
 
-$socket->on('message', function ($msg) {
-    echo $msg, PHP_EOL;
-});
-
 $socket->send('id');
+$uuid = $socket->recv();
 
-$loop->run();
+$socket->disconnect('tcp://127.0.0.1:1337');
+
+echo $uuid, PHP_EOL;
+
+
--- src/zmq-server.php
+++ src/zmq-server.php
@@ -1,33 +1,23 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$loop = React\EventLoop\Factory::create();
+use ShortFlake\IdGenerator;
+
+$loop         = React\EventLoop\Factory::create();
+$id_generator = new IdGenerator($loop);
+
 $context = new React\ZMQ\Context($loop);
-$socket = $context->getSocket(ZMQ::SOCKET_REP);
+$socket  = $context->getSocket(ZMQ::SOCKET_REP);
 $socket->bind('tcp://127.0.0.1:1337');
 
 
-
-function getId($loop)
-{
-    $deferred = new \React\Promise\Deferred();
-
-    $loop->nextTick(function () use ($deferred) {
-        $id = uniqid();
-        $deferred->resolve($id);
-    });
-
-    return $deferred->promise();
-}
-
-$socket->on('message', function ($message) use ($loop, $socket) {
-    getId($loop)->then(function ($data) use ($socket) {
-        echo memory_get_usage(true), PHP_EOL;
-
-        $socket->send($data);
-    });
+$socket->on('message', function ($message) use ($id_generator, $socket) {
+    if ('id' == $message) {
+        $id_generator->computeId()->then(function ($uuid) use ($socket) {
+            $socket->send($uuid);
+        });
+    }
 });
-
 
 
 $loop->run();
